ebdb52543482fb18ad6b620a65a626f5c6de287d
Farrellhrs <farrellhrs@gmail.com>
2024-12-29 11:44:23 +0700

    Update all changes

--- src/main/java/com/pbo/warehouse/api/services/interfaces/ProductServiceIf.java
+++ src/main/java/com/pbo/warehouse/api/services/interfaces/ProductServiceIf.java
@@ -3,9 +3,12 @@ package com.pbo.warehouse.api.services.interfaces;
 import com.pbo.warehouse.api.dto.request.GetProductsRequestDto;
 import com.pbo.warehouse.api.dto.response.GetProductResponseDto;
 import com.pbo.warehouse.api.dto.response.GetProductsResponseDto;
+import com.pbo.warehouse.api.models.Product;
 
 public interface ProductServiceIf {
     GetProductsResponseDto getProducts(GetProductsRequestDto params);
 
     GetProductResponseDto getProductById(String id);
+
+    boolean addproduct(Product product);
 }
